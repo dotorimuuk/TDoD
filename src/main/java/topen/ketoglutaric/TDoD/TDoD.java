599478b4bf98eb0e0fb3587123e0f31a75ee1b2d
your_username <98753980+paperntman@users.noreply.github.com>
2025-09-21 20:53:08 +0900

    9월 21일, 첫 커밋!

---
+++ src/main/java/topen/ketoglutaric/TDoD/TDoD.java
@@ -0,0 +1,17 @@
+package topen.ketoglutaric.TDoD;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TDoD extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
